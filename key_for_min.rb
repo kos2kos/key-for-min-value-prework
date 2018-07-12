666962504a0a8abfbc21913824f0511a1e0738f9
Nkosi Enciso Givhan <koasterman@gmail.com>
2018-07-12 16:22:31 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,4 +4,12 @@
 def key_for_min_value(name_hash)
   name_hash.collect do |key, value|
     temp
+  end
+  i = temp[0]
+  temp.each do |index|
+    if temp[index] > i
+      i = temp[index]
+    end
+  end
+  
 end
